f231678c54afbaf964f40ffaf282396a11f10b4b
Monique-S13 <moniquesilva13000@gmail.com>
2025-07-13 16:04:52 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,15 +2,15 @@ function calculoIMC(){
     const peso = parseFloat(document.getElementById("peso").value);
     const altura = parseFloat(document.getElementById("altura").value);
 
+    const resultado = document.getElementById('resultado');
+
     if (isNaN(peso) || isNaN(altura) || peso <= 0 || altura <= 0){
         resultado.style.display = "none";
         alert("Erro ao digitar o valor! Tente novamente.");
         return;
     } else{
+        const imc = peso / (altura * altura);
         resultado.innerText = imc.toFixed(2);
         resultado.style.display = "block";
     }
-
-    const imc = peso / (altura * altura);
-    const resultado = document.getElementById('resultado');
 }
